8d66c2d41a9892a82fc879d4f37bc1b718927706
hue <32443158+huedev@users.noreply.github.com>
2023-11-08 16:08:32 -0500

    Crediting Tailwind CSS with Flask tutorial

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+// Referred an online tutorial by user Sachin (https://dev.to/sachingeek/how-to-use-tailwindcss-with-flask-1hm9)
+// This tutorial helped me set up Tailwind CSS in a Flask environment
+
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
 
